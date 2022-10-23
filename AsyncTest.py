1e37c2456c149979c9c39b5ef1c2366d3ca05f25
Youri Malleck-Ahmed <youri.malleckahmed@gmail.com>
2022-10-23 18:43:49 +0400

    Async w/o Connection Pooling

---
+++ AsyncTest.py
@@ -0,0 +1,37 @@
+import time
+import asyncio
+import aiohttp
+
+start_time = time.time()
+
+async def get_pokemon(session, url, number):
+    ''' xxx  '''
+    try:
+        get_url = f'{url}/{number}' # if (number % 2) == 0 else ""
+        async with session.get(get_url) as resp:
+            pokemon = await resp.json()
+            return f"#{number} - SUCCESS - {pokemon['name']}"
+    except asyncio.TimeoutError as ex:
+        return f"#{number} - TIMEOUT - {type(ex)}"
+    except Exception as ex:
+        return f"#{number} - ERROR - {type(ex)} - {ex.args}"
+
+async def main():
+    ''' xxx  '''
+    url = 'https://pokeapi.co/api/v2/pokemon'
+
+    client_timeout = aiohttp.ClientTimeout(total=1)
+    async with aiohttp.ClientSession(timeout=client_timeout) as session:
+
+        tasks = []
+        for number in range(1, 151):
+            task = asyncio.ensure_future(get_pokemon(session, url, number))
+            task.add_done_callback(tasks.remove)
+            tasks.append(task)
+
+        original_pokemon = await asyncio.gather(*tasks)
+        for pokemon in original_pokemon:
+            print(pokemon)
+
+asyncio.run(main())
+print(f"--- {time.time() - start_time} seconds ---")
